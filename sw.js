bfc73052f9f83a7c938e7e102e6e94451db0cfcf
estudiopatitucci <patitucciestudio@gmail.com>
2026-06-19 18:51:28 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── Service Worker — Estudio Patitucci ──────────────────────────────────────
-const CACHE_NAME = 'ep-v23';
+const CACHE_NAME = 'ep-v24';
 
 const STATIC_ASSETS = [
   './styles.css',
